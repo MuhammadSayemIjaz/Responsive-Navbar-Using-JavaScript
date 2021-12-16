1b9fb4be0da5bd35ef131ab683c744b1e16689af
Muhammad Sayem Ijaz <75717799+MuhammadSayemIjaz@users.noreply.github.com>
2021-12-17 00:37:28 +0500

    Creating And Updating Navbar Code
    Creating and updating navbar code and add functioning using JavaScript

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const toggleButton = document.getElementById("toggleBtn");
-const navItem = document.getElementById("NavItems");
-function displayNavItems() {
-    navItem.style.display = "flex";
-}
+// Getting Navbar
+const Links = document.querySelector(".links");
+const toggleBtn = document.querySelector(".navToggle"); 
+
+toggleBtn.addEventListener("click", function () {
+    
+    Links.classList.toggle("showLinks");
+});
